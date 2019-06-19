02a2612db95a0a3120c043fc2801ec18e033811a
yalei <yaleidu93@gmail.com>
2019-06-19 11:59:41 +0800

    add test case

--- tests/testthat/test-rcache.R
+++ tests/testthat/test-rcache.R
@@ -29,3 +29,25 @@ test_that("test callback", {
   Sys.sleep(1)
   expect_identical(cache$get('a'), NULL)
 })
+                   
+ 
+test_that('test callback directly', {
+  cache <- Cacher$new(expire = 1)
+  key = '1231'; val = 123
+  expect_identical(cache$get_callback(key, function() val, expire=3), val)
+  expect_identical(cache$exist(key), TRUE)
+  expect_identical(cache$get(key), val)
+  
+  key2 = '456'; val2 = 456
+  expect_identical(cache$get_callback(key2, function() val2, expire=30), val2)
+  expect_identical(cache$exist(key2), TRUE)
+  expect_identical(cache$get(key2), val2)
+  
+  Sys.sleep(3)
+  # key will disappear while key2 will still alive
+  expect_identical(cache$exist(key), FALSE)
+  expect_identical(cache$get(key), NULL)
+  expect_identical(cache$exist(key2), TRUE)
+  expect_identical(cache$get(key2), val2)
+})
+
